9173a1956adff0cc54ad3e8f5be051cde67cc9c7
nero <louis.vanbiervliet@gmail.com>
2015-08-23 22:27:59 +0200

    php DatabaseInteraction class development started

---
+++ SQL/php/DatabaseInteraction.php
@@ -0,0 +1,30 @@
+<?php
+class DatabaseInteraction
+{
+
+	// variables
+	private $userList = array('root','baerto','beurs');
+	private $passwordList = array('lnrddvnc','baertdbpass','fromzerotoone');
+
+
+	private $dbName = 'oakTest';
+	private $dbHost = 'localhost';
+
+	// constructor, $dbUser is optional
+	function DatabaseInteraction($dbUser = '')
+	{
+		if(empty($dbUser))
+		{
+			
+		}
+		else
+		{
+
+		}
+
+	}
+
+
+
+}
+?>
