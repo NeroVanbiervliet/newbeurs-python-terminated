ad46fdef00fc74c9f57fd923b8970f778d584e35
nero <louis.vanbiervliet@gmail.com>
2015-08-29 11:50:48 +0200

    create_entries file added, databaseInteraction updated (method &strategy adding)

--- SQL/python/DatabaseInteraction.py
+++ SQL/python/DatabaseInteraction.py
@@ -2,143 +2,192 @@
 import MySQLdb
 import _mysql_exceptions
 
+
 class DatabaseInteraction:
-	"""A class to interact with the oak beurs database (c) Nero"""
-	
-	# variables here declared are class variables, they are common for all objects and each object has a pointer to the same value!	
-	userList = ['root','baerto','beurs']
-	passwordList = ['lnrddvnc','baertdbpass','fromzerotoone']
+    """A class to interact with the oak beurs database (c) Nero"""
 
-	# constructor
+    # variables here declared are class variables, they are common for all objects and each object has a pointer to the same value!
+    userList = ['root', 'baerto', 'beurs']
+    passwordList = ['lnrddvnc', 'baertdbpass', 'fromzerotoone']
 
-	def __init__(self, dbName, dbUser = None):
-		
-		# variables declared in the constructor are property of the object
-		self.dbName = dbName
-		self.dbHost = 'localhost'
+    # constructor
 
-		if(dbUser is None): # default account is root NEED aanpassen
-			# local var needed further in constructor
-			dbUser = 'root'
-			self.dbUser = dbUser
-		else:
-			self.dbUser = dbUser
+    def __init__(self, dbName, dbUser=None):
 
-		# get password
-		if dbUser in self.userList:
-			userIndex = self.userList.index(self.dbUser)
-			self.dbPassword = self.passwordList[userIndex]
-	
-		else: # user is not known, set password = ""
-			self.dbPassword = ""
+        # variables declared in the constructor are property of the object
+        self.dbName = dbName
+        self.dbHost = 'localhost'
+
+        if (dbUser is None):  # default account is root NEED aanpassen
+            # local var needed further in constructor
+            dbUser = 'root'
+            self.dbUser = dbUser
+        else:
+            self.dbUser = dbUser
+
+        # get password
+        if dbUser in self.userList:
+            userIndex = self.userList.index(self.dbUser)
+            self.dbPassword = self.passwordList[userIndex]
+
+        else:  # user is not known, set password = ""
+            self.dbPassword = ""
+
+    # functions
+
+    # returns the mandatory last print statement of every method
+    def getTerminator(self):
+        return "And Now His Watch is Ended"
+
+    # returns an array of all valid table names
+    def getTableNames(self):
+
+        query = "SHOW TABLES"
+        [columnNames, queryResult] = self.executeQuery(query)
+
+        for row in queryResult:
+            print row[0]
+
+        return queryResult
+
+    # returns two arrays: [columnNames,dataRows]
+    # a valid tableName is required, as can be obtained using the getTableNames() function
+    def getAllTableEntries(self, tableName):
+
+        query = "SELECT * FROM " + tableName
+        return self.executeQuery(query)
+
+    # executes a given query
+    def executeQuery(self, query):
+
+        # open connection with database
+        conn = MySQLdb.connect(host=self.dbHost, user=self.dbUser, passwd=self.dbPassword, db=self.dbName)
+        # get cursor object
+        cur = conn.cursor()
+
+        # execute query
+        cur.execute(query)
+
+        # store result in local var
+        columnNames = cur.description
+        queryResult = cur.fetchall()
+
+        # finish
+        cur.close()
+        conn.commit()
+        conn.close()
+
+        # return result
+        return [columnNames, queryResult]
+
+    # adds a stock to the stocks table
+    # examples:
+    # name = apple
+    # ticker = AAPL
+    # market = Nasdaq
+    def addStock(self, name, ticker, market):
+
+        query = ("INSERT INTO stock(name, ticker, market) "
+                 "VALUES (\'%s\',\'%s\',\'%s\');") % (name, ticker, market)
+
+        try:
+            self.executeQuery(query)
+        except _mysql_exceptions.IntegrityError:
+            print "OAK_ERROR: Creatie van nieuwe stock in de database mislukt. Ticker bestaat al in database"
+            # exception herthrowen TODO eigen exception throwen met message hierboven?
+            raise
+
+    # returns a list containing all tickers present in the stocks table
+    def getTickerList(self):
+
+        query = "SELECT ticker FROM stock"
+        [columnNames, queryResult] = self.executeQuery(query)
+
+        # gegevens herschikken
+        result = []
+        for ticker in queryResult:
+            result.append(ticker[0])
+
+        return result
+
+    # returns stock info in a dictionary
+    def getStockInfo(self, ticker):
+
+        query = "SELECT * FROM stock WHERE ticker=\'%s\'" % (ticker)
+        [columnNames, queryResult] = self.executeQuery(query)
+
+        # data in dictionary gieten
+        result = {}
+
+        # columnames structure is -> (('id', 3, 1, 11, 11, 0, 0), ('name', 253, 5, 30, 30, 0, 0), ('ticker', 253, 4, 10, 10, 0, 1)
+        for i in range(0, len(columnNames)):
+            # add to dictionary, gaat er van uit dat ticker uniek is dus enkel eerste rij van queryResult wordt onderzocht
+            result[columnNames[i][0]] = queryResult[0][i]
+
+        return result
+
+    # adds a method to the database
+    # @param name de naam van de methode, zelfde als directorynaam van methode!
+    # @param description beschrijving van de methode, inclusief de argumenten die mee moeten gegeven worden bij uitvoeren van script!
+    def addMethod(self, name, description):
+
+        query = ("INSERT INTO method(name, description) "
+                 "VALUES (\'%s\',\'%s\');") % (name, description)
+
+        try:
+            self.executeQuery(query)
+        except _mysql_exceptions.IntegrityError:
+            print "OAK_ERROR: Creatie van nieuwe stock in de database mislukt. Methodenaam bestaat al in database"
+            # exception herthrowen TODO eigen exception throwen met message hierboven
+            raise
+
+    # returns method info in a dictionary
+    def getMethodInfo(self, name):
+
+        query = "SELECT * FROM method WHERE name=\'%s\'" % (name)
+        [columnNames, queryResult] = self.executeQuery(query)
+
+        # data in dictionary gieten
+        result = {}
+
+        # columnames structure is -> (('id', 3, 1, 11, 11, 0, 0), ('name', 253, 5, 30, 30, 0, 0), ('ticker', 253, 4, 10, 10, 0, 1)
+        for i in range(0, len(columnNames)):
+            # add to dictionary, gaat er van uit dat ticker uniek is dus enkel eerste rij van queryResult wordt onderzocht
+            result[columnNames[i][0]] = queryResult[0][i]
+
+        return result
+
+    # adds a user to the database
+    # only use this in development! a hashed password is required, but this must be generate by a JSP script using bcrypt
+    # users must be added in the web application, not in python
+    def addUser(self, userName, passwordHashed):
+
+        query = ("INSERT INTO user(name, passwordHashed) "
+                 "VALUES (\'%s\',\'%s\');") % (userName, passwordHashed)
+
+        try:
+            self.executeQuery(query)
+        except _mysql_exceptions.IntegrityError:
+            print "OAK_ERROR: Creatie van nieuwe user in de database mislukt. UserName bestaat al in database"
+            # exception herthrowen TODO eigen exception throwen met message hierboven
+            raise
 
 
-	# functions
 
-	# returns the mandatory last print statement of every method 
-	def getTerminator(self):
-		return "And Now His Watch is Ended"
+    # adds a strategy to the database
+    # TODO also add entry in strategyEditHistory
+    def addStrategy(self, strategyName, methodName, parameters):
 
-	# returns an array of all valid table names
-	def getTableNames(self):
-		
-		query = "SHOW TABLES"	
-		[columnNames, queryResult] = self.executeQuery(query)
+        # method info ophalen om id te vinden uit name
+        # NEED exception voor als method niet voorkomt in db
+        methodInfo = self.getMethodInfo(methodName)
 
-		for row in queryResult:	
-			print row[0]	
+        query = ("INSERT INTO strategy(name, method, parameters) "
+                 "VALUES (\'%s\',\'%s\',\'%s\');") % (strategyName, methodInfo.get('id'), parameters)
 
-		return queryResult	
-
-	# returns two arrays: [columnNames,dataRows]
-	# a valid tableName is required, as can be obtained using the getTableNames() function
-	def getAllTableEntries(self, tableName):
-
-		query = "SELECT * FROM " + tableName
-		return self.executeQuery(query)
-		
-	# executes a given query	
-	def executeQuery(self, query):
-
-		# open connection with database		
-		conn = MySQLdb.connect(host=self.dbHost, user=self.dbUser, passwd=self.dbPassword, db=self.dbName)
-		# get cursor object
-		cur = conn.cursor() 
-
-		# execute query
-		cur.execute(query)
-
-		# store result in local var
-		columnNames = cur.description
-		queryResult = cur.fetchall()
-
-		# finish
-		cur.close()
-		conn.commit()
-		conn.close()
-
-		# return result
-		return [columnNames,queryResult]
-
-
-	# adds a stock to the stocks table
-	# examples:
-	# name = apple
-	# ticker = AAPL
-	# market = Nasdaq
-	def addStock(self, name, ticker, market):
-		
-		query = ("INSERT INTO stock(name, ticker, market) "
-			"VALUES (\'%s\',\'%s\',\'%s\');") % (name,ticker,market)
-		
-		try:	
-			self.executeQuery(query)
-		except _mysql_exceptions.IntegrityError:
-			print "OAK_ERROR: Creatie van nieuwe stock in de database mislukt. Ticker bestaat al in database"
-			# exception herthrowen TODO eigen exception throwen met message hierboven? 	
-			raise
-	
-	# returns a list containing all tickers present in the stocks table	
-	def getTickerList(self):
-		
-		query = "SELECT ticker FROM stock"
-		[columnNames,queryResult] = self.executeQuery(query)
-		
-		# gegevens herschikken
-		result = []		
-		for ticker in queryResult:
-			result.append(ticker[0])
-
-		return result
-
-	# returns stock info in a dictionary
-	def getStockInfo(self, ticker):
-
-		query = "SELECT * FROM stock WHERE ticker=\'%s\'" % (ticker)
-		[columnNames,queryResult] = self.executeQuery(query)
-
-		# data in dictionary gieten
-		result = {}		
-		
-		# columnames structure is -> (('id', 3, 1, 11, 11, 0, 0), ('name', 253, 5, 30, 30, 0, 0), ('ticker', 253, 4, 10, 10, 0, 1)
-		for i in range(0,len(columnNames)):
-			# add to dictionary, gaat er van uit dat ticker uniek is dus enkel eerste rij van queryResult wordt onderzocht
-			result[columnNames[i][0]] = queryResult[0][i]
-
-		return result
-
-	# adds a method to the database
-	# @param name de naam van de methode, zelfde als directorynaam van methode!
-	# @param description beschrijving van de methode, inclusief de argumenten die mee moeten gegeven worden bij uitvoeren van script!
-	def addMethod(self, name, description):
-		
-		query = ("INSERT INTO method(name, description) "
-			"VALUES (\'%s\',\'%s\');") % (name,description)
-
-		try:	
-			self.executeQuery(query)
-		except _mysql_exceptions.IntegrityError:
-			print "OAK_ERROR: Creatie van nieuwe stock in de database mislukt. Methodenaam bestaat al in database"
-			# exception herthrowen TODO eigen exception throwen met message hierboven
-			raise
-
+        try:
+            self.executeQuery(query)
+        except _mysql_exceptions.IntegrityError:
+            print "OAK_ERROR: Creatie van nieuwe strategy in de database mislukt. StrategyName bestaat al in database"
+            # exception herthrowen TODO eigen exception throwen met message hierboven
+            raise
---
+++ SQL/python/create_entries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from DatabaseInteraction import DatabaseInteraction
+
+print 'Test entries will be added to database'
+
+dbObject = DatabaseInteraction('backtest_real')
+
+
+# users
+# pass fromzerotoone
+dbObject.addUser("beurs","$2a$12$rBOjgBqaorleheA2XuospuN65sO5XsUUHddygO4z8Hcd2eIA.vNPe")
+
+# stocks
+dbObject.addStock("apple","AAPL","Nasdaq")
+dbObject.addStock("oak","OAK","Euronext")
+
+
+# methods
+dbObject.addMethod("fundamentalist","geen argumenten jongeuh")
+dbObject.addMethod("indexvolger","geef als string de index mee die je wil volgen")
+
+# strategies
+
+dbObject.addStrategy("fundamentAgressief","fundamentalist","")
+dbObject.addStrategy("bel20volger","indexvolger","bel20")
+
+print 'Done'
