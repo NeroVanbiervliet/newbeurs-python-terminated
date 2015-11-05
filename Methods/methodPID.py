2eda235a81b2382e8d65255528b2e920d3fa4bce
Matthias Baert <matthias.baert1@gmail.com>
2015-11-05 16:20:34 +0100

    Fix voor vreemde problemen

--- Methods/methodPID.py
+++ Methods/methodPID.py
@@ -11,15 +11,15 @@ def generateData(tickerList):
             
     return stockDataDict
 
-def mainBuy(date,stockDataDict,tickerList,buyParameters):
+def mainBuy(date,stockDataDict,tickerList,parameters):
     """ Dit is methode 1 die aandelen koopt en verkoopt onder bepaalde voorwaardes
         Input: date = welke dag geanalyseerd moet worden
                
         Output: buyList = zegt welke aandelen gekocht worden en voor hoe lang
         """
 
-    notyetdefined = buyParameters[0]
-    duration = buyParameters[1]
+    notyetdefined = eval(parameters[0])
+    duration = eval(parameters[1])
     
     buyList = []
     
@@ -40,7 +40,7 @@ def mainBuy(date,stockDataDict,tickerList,buyParameters):
 
     return buyList
 
-def mainSell(date,stockDataDict,tickerList,sellParameters,portfolio):
+def mainSell(date,stockDataDict,tickerList,parameters,portfolio):
 
     transactionList = []
     indices = []
